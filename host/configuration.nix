682254e4e1677ae45978862e6eda6688f43089a1
zvasoup <zulfa.plank@proton.me>
2024-04-01 22:36:40 +0800

    location and time module

--- host/configuration.nix
+++ host/configuration.nix
@@ -10,6 +10,7 @@
     # ./main-user.nix
     inputs.home-manager.nixosModules.default
     ./../system/hardware/bootloader.nix
+    ./../system/hardware/location-and-time.nix.nix
   ];
 
   # main-user.enable = true;
@@ -26,22 +27,6 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "Asia/Singapore";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
