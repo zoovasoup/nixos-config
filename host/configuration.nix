d782477a6934ecc07e6029b939a07db4a0907999
zvasoup <zulfa.plank@proton.me>
2024-04-02 08:35:37 +0800

    xserver with gnome module

--- host/configuration.nix
+++ host/configuration.nix
@@ -20,13 +20,6 @@
   # main-user.enable = true;
   # main-user.userName = "zvasoup";
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
