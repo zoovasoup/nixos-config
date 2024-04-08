040f48902bae52ee51670af5a785232715df56dd
zvasoup <zulfa.plank@proton.me>
2024-04-08 22:01:29 +0800

    nvim lsp and completion

--- misterio-config/home-manager/nixvim/plugins/default.nix
+++ misterio-config/home-manager/nixvim/plugins/default.nix
@@ -1,7 +1,22 @@
 { ... } : {
   programs.nixvim.plugins = {
+    lazy.enable = true;
+
     lualine.enable = true; 
     treesitter.enable = true;
+    comment.enable = true;
+    surround.enable = true; 
+    harpoon.enable = true; 
+
+    telescope = {
+      enable = true; 
+      extension.fzy-native.enable = true;
+    };
+
+    undotree.enable = true;
+    fugitive.enable = true;
+    oil.enable = true;
+
     lsp = {
       enable = true;
       servers = {
@@ -9,6 +24,18 @@
         tsserver.enable = true;
         lua-ls.enable = true; 
       };
+    };
+    
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+      sources = [
+       {name = "nvim_lsp";}
+       {name = "path";}
+       {name = "buffer";}
+       {name = "luasnip";}
+       {name = "lua";}
+      ];
     };
   };
 }
