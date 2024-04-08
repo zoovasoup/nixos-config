c152cb7af25954d7d0a6308a2a4d3587529e5386
zvasoup <zulfa.plank@proton.me>
2024-04-08 20:29:31 +0800

    adding lsp

--- misterio-config/home-manager/nixvim/plugins/default.nix
+++ misterio-config/home-manager/nixvim/plugins/default.nix
@@ -1,5 +1,14 @@
 { ... } : {
   programs.nixvim.plugins = {
     lualine.enable = true; 
+    tresitter.enable = true;
+    lsp = {
+      enable = true;
+      servers = {
+        nil_ls.enable = true; 
+        tsserver.enable = true;
+        lua-ls.enable = true; 
+      };
+    };
   };
 }
