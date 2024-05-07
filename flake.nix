a735872f3fbe02e111dbc5a809a7a4c5c35aa044
zvasoup <zulfa.plank@proton.me>
2024-05-07 22:45:15 +0800

    adding hy3 plugin to hyprland

--- flake.nix
+++ flake.nix
@@ -16,14 +16,14 @@
     };
 
     #hyprland.url = "github:hyprwm/Hyprland";
-    hyprland.url = "github:hyprwm/Hyprland?ref=v0.38.0";
+    hyprland.url = "github:hyprwm/Hyprland?ref=v0.39.1";
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
     };
     
     hy3 = {
-      url = "github:outfoxxed/hy3?ref=hl0.38.0"; # where {version} is the hyprland release version
+      url = "github:outfoxxed/hy3?ref=hl0.39.1"; # where {version} is the hyprland release version
       inputs.hyprland.follows = "hyprland";
     };
 
