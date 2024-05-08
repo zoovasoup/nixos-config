4380d26e11578e5cec79ebc6111268961d97f126
zvasoup <zulfa.plank@proton.me>
2024-05-08 18:48:50 +0800

    quiet boot

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -43,6 +43,21 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.loader.systemd-boot.configurationLimit = 15;
+  boot.loader.systemd-boot.consoleMode = "max";
+
+  boot.plymouth.enable = true;
+  boot.loader.timeout = 1;
+  boot.kernelParams = [
+        "quiet"
+      "loglevel=3"
+      "systemd.show_status=auto"
+      "udev.log_level=3"
+      "rd.udev.log_level=3"
+      "vt.global_cursor_default=0"
+  ];
+  boot.consoleLogLevel = 0;
+
 # ==================
 
   time.timeZone = "Asia/Singapore";
