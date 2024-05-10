1395ece64a89f668c76423bc7842fa050c05f502
zvasoup <zulfa.plank@proton.me>
2024-05-10 18:10:00 +0800

    adding subversion

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -80,7 +80,16 @@
   services.xserver.enable = true;
 
 # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm = {
+    enable = true;
+    settings = {
+      daemon = {
+        # AutomaticLogin = "zvasoup";
+        # AutomaticLoginEnable = true;
+        WaylandEnable = true;
+      };
+    };
+  };
   services.xserver.desktopManager.gnome.enable = true;
 
 # Configure keymap
@@ -184,6 +193,7 @@
         # vimPlugins.harpoon2
         udiskie
         arduino
+        subversion
       ];
     };
   };
