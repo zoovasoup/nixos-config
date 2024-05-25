84e9fadc163e930fd6bb9070333e70020dc3cdf5
zvasoup <zulfa.plank@proton.me>
2024-05-25 22:48:43 +0800

    changing hyprland flake

--- flake.nix
+++ flake.nix
@@ -15,14 +15,16 @@
     };
 
     #hyprland.url = "github:hyprwm/Hyprland";
-    hyprland.url = "github:hyprwm/Hyprland?ref=v0.39.1";
+    #hyprland.url = "github:hyprwm/Hyprland?ref=v0.39.1";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
     };
     
     hy3 = {
-      url = "github:outfoxxed/hy3?ref=hl0.39.1"; # where {version} is the hyprland release version
+      #url = "github:outfoxxed/hy3?ref=hl0.39.1"; # where {version} is the hyprland release version
+      url = "github:outfoxxed/hy3?ref=hl0.40.0"; # where {version} is the hyprland release version
       inputs.hyprland.follows = "hyprland";
     };
 
