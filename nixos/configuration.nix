b2fa61839550d346f6b9762b2b9d2e879eb76771
zvasoup <zulfa.plank@proton.me>
2024-06-06 11:13:29 +0800

    changing auto-cpufreq to tlp

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -269,18 +269,35 @@
     enable = false;
   };
 
-  programs.auto-cpufreq = {
+  services.tlp = {
     enable = true;
     settings = {
-      battery = {
-        governor = "powersave";
-        turbo = "never";
-        enable_thresholds = true;
-        start_threshold = "65";
-        stop_threshold = "81";
-      };
+      TLP_DEFAULT_MODE = "bat";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_SCALING_MAX_FREQ_ON_BAT = "1700000";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_BOOST_ON_BAT = 0;
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+      RADEON_DPM_PERF_LEVEL_ON_BAT = "low";
+      RADEON_DPM_STATE_ON_BAT = "battery";
+      USB_AUTOSUSPEND = 0;
+      START_CHARGE_THRESH_BAT0 = "70";
+      STOP_CHARGE_THRESH_BAT0 = "81";
     };
   };
+
+  # programs.auto-cpufreq = {
+  #   enable = true;
+  #   settings = {
+  #     battery = {
+  #       governor = "powersave";
+  #       turbo = "never";
+  #       enable_thresholds = true;
+  #       start_threshold = "65";
+  #       stop_threshold = "81";
+  #     };
+  #   };
+  # };
 
   programs.nix-ld = {
     enable = true;
