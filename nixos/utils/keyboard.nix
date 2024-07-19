da42dd55356671c2b2907694e4d5f3036380df20
zvasoup <zulfa.plank@proton.me>
2024-07-19 08:52:45 +0800

    adding udev for vial

--- nixos/utils/keyboard.nix
+++ nixos/utils/keyboard.nix
@@ -108,4 +108,12 @@
       };
     };
   };
+
+  # udev settup for vial
+  services.udev = {
+    enable = true;
+    extraRules = ''
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+    '';
+  };
 }
