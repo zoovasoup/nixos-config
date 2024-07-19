e6d6d715aff4152c717fc0e15a7b4f5f19c8d203
zvasoup <zulfa.plank@proton.me>
2024-07-19 08:57:03 +0800

    auto enable and auto reset bluetooth

--- nixos/system/bluetooth.nix
+++ nixos/system/bluetooth.nix
@@ -1,10 +1,25 @@
-{...}: {
+{pkgs, ...}: let
+  restartBluetoothScript = ''
+    #!${pkgs.bash}/bin/bash
+    ${pkgs.systemd}/bin/systemctl restart bluetooth.service
+  '';
+in {
   hardware.bluetooth = {
     enable = true;
     settings = {
       General = {
         experimental = true;
       };
+      AutoEnable = true;
     };
+  };
+
+  systemd.services.restart-bluetooth = {
+    description = "Restart Bluetooth after resume";
+    after = ["hibernate.target" "suspend.target" "sleep.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.writeScriptBin "restart-bluetooth" restartBluetoothScript}/bin/restart-bluetooth";
+    };
+    wantedBy = ["hibernate.target" "suspend.target" "sleep.target"];
   };
 }
