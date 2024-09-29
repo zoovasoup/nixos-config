701cfc00264d42b797fd4652c3c85d9430df2a25
zvasoup <zulfa.plank@proton.me>
2024-09-29 09:51:04 +0800

    touchpad driver

--- nixos/utils/touchpad.nix
+++ nixos/utils/touchpad.nix
@@ -1,4 +1,7 @@
 {...}: {
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
+  services.libinput.mouse.accelProfile = "flat";
+  services.libinput.touchpad.accelProfile = "flat";
+  # services.xserver.synaptics.enable = true;
 }
